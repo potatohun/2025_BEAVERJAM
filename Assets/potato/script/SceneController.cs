2743c550d6ae3b8361f9b31ffb07cc3585f4b11d
potato_hun <107704298+potatohun@users.noreply.github.com>
2025-10-26 10:20:15 +0900

    1026/1000
    빌드 버전

--- Assets/potato/script/SceneController.cs
+++ Assets/potato/script/SceneController.cs
@@ -6,4 +6,10 @@ public class SceneController : MonoBehaviour
     public void OnClickStartButton(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player") {
+            OnClickStartButton("InGame");
+        }
+    }
 }
