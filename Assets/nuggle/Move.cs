b43d534cb6ed27ce9bb97c4c43ac21761c9780d5
joojooo1 <143375228+joojooo1@users.noreply.github.com>
2025-11-23 14:07:01 +0900

    move. player prefab 수정
    move. player prefab 수정

--- Assets/nuggle/Move.cs
+++ Assets/nuggle/Move.cs
@@ -14,10 +14,13 @@ public class Move : MonoBehaviour
     public LayerMask groundLayerMask;
     public float groundCheckDistance = 1f;
     public Transform groundCheckPoint; // 레이캐스트 시작점
-    
+
     //public LayerMask waterLayerMask;
     //public bool Playerinwater = false;
-
+    [Header("Overhead Check")]
+    public LayerMask OverheadLayerMask;
+    public float OverheadCheckDistance = 1f;
+    public Transform OverheadCheckPoint; // 레이캐스트 시작점
 
     [Header("State Management")]
     public bool isDead = false;
@@ -73,6 +76,15 @@ public class Move : MonoBehaviour
             groundCheckObj.transform.localPosition = new Vector3(0, -0.5f, 0);
             groundCheckPoint = groundCheckObj.transform;
         }
+
+        if (OverheadCheckPoint == null)
+        {
+            GameObject OverheadCheckObj = new GameObject("OverheadCheckPoint");
+            OverheadCheckObj.transform.SetParent(transform);
+            OverheadCheckObj.transform.localPosition = new Vector3(0, 0.5f, 0);
+            OverheadCheckPoint = OverheadCheckObj.transform;
+        }
+        
     }
 
     //private void Update()
@@ -94,6 +106,7 @@ public class Move : MonoBehaviour
     //}
     void LateUpdate()
     {
+        if (CheckOverhead()) SetDead();
         // 죽은 상태나 대화 중이면 입력 무시
         if (isDead || isInDialogue) return;
 
@@ -317,6 +330,26 @@ public class Move : MonoBehaviour
 
 
         return grounded;
+    }
+
+    private bool CheckOverhead()
+    {
+        if (groundCheckPoint == null) return false;
+        if (FriendManager.FM.isPlayerInWater) return false;
+
+        // 발 아래로 레이캐스트 발사
+        Vector2 rayOrigin = OverheadCheckPoint.position;
+        Vector2 rayDirection = Vector2.up;
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, OverheadCheckDistance, OverheadLayerMask);
+
+        bool overhead = hit.collider != null;
+        // 디버그용 레이캐스트 시각화
+        Debug.DrawRay(rayOrigin, rayDirection * OverheadCheckDistance, overhead ? Color.green : Color.red);
+
+
+
+        return overhead;
     }
 
     // 죽음 트리거 처리 (상대편이 Fire(6번)나 Obstacle(7번) 레이어면 죽음)
